946a74ccbf81b66f3b4caea2411b92b31c9de947
Luís Fernando Arnoni Fritzen <52474261+Luis-Kendelick@users.noreply.github.com>
2024-03-31 03:52:45 +0000

    fix: ? for !

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 const Home = () => {
     return(
         <>
-        <h1>Você está cheirosa?</h1>
-        <h1>Que gotoso</h1>
+            <h1>Você está cheirosa!</h1>
+            <h1>Que gotoso</h1>
         </>
     ) 
 }
